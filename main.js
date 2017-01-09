1fcdfc7e8b2d3a0e5b739ace69f351a58e67d186
jmk2142 <jmk2142@tc.columbia.edu>
2017-01-09 16:43:22 -0500

    Added annotations to data architecture

--- main.js
+++ main.js
@@ -1,77 +1,83 @@
-var youtube{
-	videoURL: "http://xxxx.com",
-	title: "mozart"
-	user:{
-		username:"xx",
-		userprofileimageURL:"http://xxx",
-		numberSubscriber: 15256,
-		subscription: function // action to subscribe the user
-	}
-	subscribe: function,
-	numberViews: 3982196 + "views",
-	numberLike:16866,
-	Like: function,
-	numberDislike: 645
-	dislike: function,
-	videodecription: "xxxxxxxxx",
+var userA = {};
+var userB = {};
+var userC = {};
 
-	relatedVideos: {
-		videoA {
+var youtube = {
+	videoURL: "http://xxxx.com",
+	title: "mozart",
+	user: {
+		username:"xx",
+		userprofileimageURL:"http://xxx", // userProfileImageURL
+		numberSubscriber: 15256, // since plural, numberSubscribers
+		subscription: function(){} // if it's an action, use action word - subscribe
+	},
+	subscribe: function(){}, // You can be specific and DISTINCT e.g. subscribeChannel and followUser
+	numberViews: 3982196 + "views", // Think about this as a number not full message string - we'll want to ADD numbers. Datatypes and use of.
+	numberLike: 16866,
+	Like: function(){}, // Lowercase like()
+	numberDislike: 645, // plural
+	dislike: function(){},
+	videodecription: "xxxxxxxxx", // camelCase
+
+	relatedVideos: { // For lists that will continue to grow - use Arrays ... relatedVideos: []
+		videoA: {
+			videoURL: "http://xxx",
+			description: "xxx",
+		},
+		videoB: {
+			videoURL: "http://xxx",
+			description: "xxx",
+		},
+		videoC: {
 			videoURL: "http://xxx",
 			description: "xxx",
 		}
-		videoB {
-			videoURL: "http://xxx",
-			description: "xxx",
-		}
-		videoC {
-			videoURL: "http://xxx",
-			description: "xxx",
-		}
-	}
+	},
 
 	comment: {
 		numbercomment: 1766,
+		lastComment: new Date(),
 
-		usersA{
-			userprofile: userA,
-			comment: "xxxx",
-			like: function,
-			dislike:function,
-			report:function,
-			reply: function // action to reply
-			user: {
-				replycomment: "xxx"
-				rely: function
+		commentList: [ // Try to think in terms of lists (arrays) of similar objects
+			{
+				userprofile: userA,
+				comment: "xxxx",
+				like: function(){},
+				dislike:function(){},
+				report:function(){},
+				reply: function(){}, // action to reply
+				user: {
+					replycomment: "xxx",
+					reply: function(){}
+				}
+			},
+			{
+				userprofile: userB,
+				comment: "xxxx",
+				like: function(){},
+				dislike:function(){},
+				report:function(){},
+				reply: function(){}, // action to reply
+				user: {
+					replycomment: "xxx",
+					rely: function(){}
+				}
+			},
+			{
+				userprofile: userC,
+				comment: "xxxx",
+				like: function(){},
+				dislike:function(){},
+				report:function(){},
+				reply: function(){}, // action to reply
+				user: {
+					replycomment: "xxx",
+					rely: function(){}
+				}
 			}
-		}
-
-		usersB{
-			userprofile: userB,
-			comment: "xxxx",
-			like: function,
-			dislike:function,
-			report:function,
-			reply: function // action to reply
-			user: {
-				replycomment: "xxx"
-				rely: function
-			}
-		}
-
-
-		usersC{
-			userprofile: userC,
-			comment: "xxxx",
-			like: function,
-			dislike:function,
-			report:function,
-			reply: function // action to reply
-			user: {
-				replycomment: "xxx"
-				rely: function
-			}
-		}
-
+		]
 	}
 }
+
+
+var x = [{a:"xxx"}, function(){ console.log("Hola!"); }];
