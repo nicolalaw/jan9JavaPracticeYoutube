cf7e1e33bc297ee1a260e016a9607ae3d8203a83
YuenLam Law <yll2110@Jins-iMac.local>
2017-01-09 15:17:58 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,77 @@
+var youtube{
+	videoURL: "http://xxxx.com",
+	title: "mozart"
+	user:{
+		username:"xx",
+		userprofileimageURL:"http://xxx",
+		numberSubscriber: 15256,
+		subscription: function // action to subscribe the user
+	}
+	subscribe: function,
+	numberViews: 3982196 + "views",
+	numberLike:16866,
+	Like: function,
+	numberDislike: 645
+	dislike: function,
+	videodecription: "xxxxxxxxx",
+
+	relatedVideos: {
+		videoA {
+			videoURL: "http://xxx",
+			description: "xxx",
+		}
+		videoB {
+			videoURL: "http://xxx",
+			description: "xxx",
+		}
+		videoC {
+			videoURL: "http://xxx",
+			description: "xxx",
+		}
+	}
+
+	comment: {
+		numbercomment: 1766,
+
+		usersA{
+			userprofile: userA,
+			comment: "xxxx",
+			like: function,
+			dislike:function,
+			report:function,
+			reply: function // action to reply
+			user: {
+				replycomment: "xxx"
+				rely: function
+			}
+		}
+
+		usersB{
+			userprofile: userB,
+			comment: "xxxx",
+			like: function,
+			dislike:function,
+			report:function,
+			reply: function // action to reply
+			user: {
+				replycomment: "xxx"
+				rely: function
+			}
+		}
+
+
+		usersC{
+			userprofile: userC,
+			comment: "xxxx",
+			like: function,
+			dislike:function,
+			report:function,
+			reply: function // action to reply
+			user: {
+				replycomment: "xxx"
+				rely: function
+			}
+		}
+
+	}
+}
